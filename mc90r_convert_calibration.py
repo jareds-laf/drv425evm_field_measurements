d325163327a729c9eed18dc34bac46e068ed4c87
jareds-laf <sofairj@lafayette.edu>
2024-07-25 00:51:31 -0700

    Lots of changes
    Beginning to add functionality of MC90R data reading. Started by creating a quick script that converts the calibration data in mG to mT

---
+++ mc90r_convert_calibration.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import os
+import pint
+
+# Define the unit registry
+ureg = pint.UnitRegistry(auto_reduce_dimensions=True)
+
+def normalize_path(in_path):
+    # A quick function to ensure that any input paths are properly referenced
+    return os.path.normpath(os.path.realpath(os.path.expanduser(in_path)))
+
+# Read the MC90R calibration data
+calibration_path = normalize_path(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mc90r_calibration.csv'))
+print(calibration_path)
+calibration_data = pd.read_csv(calibration_path)
+
+print(calibration_data.head())
+
+# Convert mV/mG to mV/mT
+calibration_data.loc[:, 'Vo/Field (mV/mG)'] = calibration_data['Vo/Field (mV/mG)'] * 10**4
+
+# Convert mG/mV to mT/mV
+calibration_data.loc[:, 'Field/Vo (mG/mV)'] = calibration_data['Field/Vo (mG/mV)'] / 10**4
+
+
+# Rename columns appropriately
+calibration_data.rename(columns={'Vo/Field (mV/mG)': 'Vo/Field (mV/mT)', 'Field/Vo (mG/mV)': 'Field/Vo (mT/mV)'}, inplace=True)
+
+print(calibration_data.head())
+
+# Save the modified DataFrame to a new CSV file
+output_file_path = normalize_path(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mc90r_calibration_teslas.csv'))
+print(f'Saving the calibration data to {output_file_path}')
+calibration_data.to_csv(output_file_path, index=False, header=True)
--- mc90r_measurements.py
+++ mc90r_measurements.py
@@ -9,32 +9,24 @@ def normalize_path(in_path):
     # A quick function to ensure that any input paths are properly referenced
 	return os.path.normpath(os.path.realpath(os.path.expanduser(in_path)))
 
-# Read the CSV file
+# Read the oscilloscope data file and MC90R calibration data
 csv_path = normalize_path(r'G:\My Drive\Other\REUs\Summer 2024\UCD\Scope Data\ALL0006\F0006CH2.CSV')
-# csv_path = normalize_path('D:\ALL0006\F0006CH2.CSV')
 scope_data = pd.read_csv(csv_path, header=None)
 scope_data.rename(columns={3: "Time (s)", 4: "V_out (V)"}, inplace=True)
+
+calibration_path = normalize_path(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mc90r_calibration.csv'))
+calibration_data = pd.read_csv(calibration_path)
 
 # Subtract the vertical offset
 vertical_offset = pd.to_numeric(scope_data[1][9])  # B10 corresponds to 1, 9 in 0-based index
 scope_data.insert(6, column='V_out - Offset (V)', value=scope_data['V_out (V)'] - vertical_offset)
 
-# Populate G1:I1 with editable values
-# For the most part, G and Gfg should not change
-R_shunt = 100 * ureg.ohm
-G = 4 * ureg.V / ureg.V
-Gfg = 12.2 * ureg.mA / ureg.mT
-
-scope_data.at[19, 0] = 'R_shunt (Ohms)'
-scope_data.at[19, 1] = R_shunt.magnitude
-scope_data.at[20, 0] = 'G (V/V)'
-scope_data.at[20, 1] = G.magnitude
-scope_data.at[21, 0] = 'Gfg (mA/mT)'
-scope_data.at[21, 1] = Gfg.magnitude
-
-print(f"{R_shunt}\n{G}\n{Gfg}")
+# Convert Vo to field using the calibration data
+print(calibration_data.head())
+# print(calibration_data['Vo/Field (mV/mG)'] * ureg.mG)
 
 
+scope_data.insert(7, column='B (T)', value=0.0)
 
 # Save the modified DataFrame to a new CSV file
 output_file_path = normalize_path(r'G:\My Drive\Other\REUs\Summer 2024\UCD\Scope Data\ALL0005\OUT_F0005CH1.csv')
